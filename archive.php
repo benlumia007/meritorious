a3d42de01774eb327d701cd3b5f55421d501c6f4
Benjamin Lu <benlumia007@gmail.com>
2018-12-14 07:36:26 -0800

    updated

--- archive.php
+++ archive.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Initiator (archive.php)
+ * Meritorious (archive.php)
  *
- * @package     Initiator Starter Theme
+ * @package     Meritorious
  * @copyright   Copyright (C) 2018. Benjamin Lu
  * @license     GNU General Public License v2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
  * @author      Benjamin Lu (https://getbenonit.com)
