c34beb9abb051ab1511d054b40159b418b54e657
Benjamin Lu <benlumia007@gmail.com>
2018-12-13 17:18:10 -0800

    rename functions to meritorious

--- includes/custom-background.php
+++ includes/custom-background.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Initiator (custom-background.php)
+ * Meritorious (custom-background.php)
  *
- * @package     Initiator
+ * @package     Meritorious
  * @copyright   Copyright (C) 2018. Benjamin Lu
  * @license     GNU General Public License v2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
  * @author      Benjamin Lu (https://getbenonit.com)
@@ -17,10 +17,10 @@
 /**
  *  1.0 - Includes (Custom Background)
  */
-function initiator_load_custom_background() {
+function meritorious_load_custom_background() {
 	$args = array(
 		'default-color' => 'ffffff',
 	);
 	add_theme_support( 'custom-background', $args );
 }
-add_action( 'after_setup_theme', 'initiator_load_custom_background' );
+add_action( 'after_setup_theme', 'meritorious_load_custom_background' );
